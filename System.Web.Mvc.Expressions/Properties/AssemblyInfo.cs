55490938a50d04631accfaba36c4fdac4a5a804b
Dennis <dennis.lems@coas.com>
2019-05-20 15:20:34 +0200

    updated packages
    bumped to 1.0.7
    upgrade to .net 4.6.1
    updated ignore

--- System.Web.Mvc.Expressions/Properties/AssemblyInfo.cs
+++ System.Web.Mvc.Expressions/Properties/AssemblyInfo.cs
@@ -2,3 +2,6 @@
 
 [assembly: AssemblyTitle("System.Web.Mvc.Expressions")]
 [assembly: AssemblyProduct("System.Web.Mvc.Expressions")]
+[assembly: AssemblyVersion("1.0.7.0")]
+[assembly: AssemblyFileVersion("1.0.7.0")]
+
